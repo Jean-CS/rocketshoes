9a5e327573fd84c4705684e193505ea98840aeb7
Jean <jean.carlos@fullbar.com.br>
2019-09-27 23:44:04 -0300

    Checking if item stock is available and not adding it again if it was already in state

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,13 +1,38 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ payload }) {
-  const res = yield call(api.get, `/products/${payload}`);
+  const productExists = yield select(state => state.cart.find(p => p.id === id));
 
-  yield put(addToCartSuccess(res.data));
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERRO');
+    return;
+  }
+
+  if (productExists) {
+    yield put(updateAmount(id, amount));
+  } else {
+    const res = yield call(api.get, `/products/${payload}`);
+
+    const data = {
+      ...res.data,
+      amount: 1,
+      priceFormatted: formatPrice(res.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
